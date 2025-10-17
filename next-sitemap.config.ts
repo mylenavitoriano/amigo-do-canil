886cf690615caf6358660ce5df666726e5f64636
Mylena <mylenabarbosa08@gmail.com>
2025-10-17 15:06:43 -0300

    feat: criado sitemap

---
+++ next-sitemap.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://amigodocanil.com.br",
+  generateRobotsTxt: true, // também cria o robots.txt automaticamente
+  changefreq: "weekly",
+  priority: 0.7,
+  sitemapSize: 5000,
+};
