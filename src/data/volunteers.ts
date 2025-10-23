c622241852253523e2ce739af50ad22a2e65faaf
Mylena Vitoriano <71454293+mylenavitoriano@users.noreply.github.com>
2025-10-23 10:30:27 -0300

    Update volunteer images to use Cloudinary links

--- src/data/volunteers.ts
+++ src/data/volunteers.ts
@@ -2,46 +2,51 @@ export const listVolunteers = [
   {
     id: 0,
     name: "Yasmin",
-    image: "/people/yasmin-voluntaria.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226006/yasmin_ln3c1e.jpg",
   },
   {
     id: 1,
     name: "Jorge",
-    image: "/people/jorge-voluntario.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226009/jorge_pzmtya.jpg",
   },
   {
     id: 2,
     name: "Eduardo",
-    image: "/people/eduardo-voluntario.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226005/eduardo_wclnwn.jpg",
   },
   {
     id: 3,
     name: "Pablo",
-    image: "/people/pablo-voluntario.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226012/pablo_b2etjh.jpg",
   },
   {
     id: 4,
     name: "Mylena",
-    image: "/people/mylena-voluntaria.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226004/mylena_zdz0p6.jpg",
   },
   {
     id: 5,
     name: "Lucas",
-    image: "/people/lucas-voluntario.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226008/lucas_msd3jh.jpg",
   },
   {
     id: 6,
     name: "Nádia",
-    image: "/people/nadia-voluntaria.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226003/nadia_l29heg.jpg",
   },
   {
     id: 7,
     name: "Suellen",
-    image: "/people/suellen-voluntaria.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226000/suellen_vwglej.jpg",
   },
   {
     id: 8,
     name: "Rose",
-    image: "/people/rosa-voluntaria.jpg",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226001/rosa_jzcqns.jpg",
+  },
+  {
+    id: 9,
+    name: "Anderson e Andreia",
+    image: "https://res.cloudinary.com/dyju9midi/image/upload/v1761226104/anderson-andrea_p9ctgi.jpg",
   },
 ];
